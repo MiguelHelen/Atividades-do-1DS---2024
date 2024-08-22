5bd90c5b1354c4b616fb08377b63ae877f0103ea
Miguel Heleno <miguelheleno468@gmail.com>
2024-08-22 07:59:34 -0300

    .

--- TPA/Terceiro Bimestre/Exercicios da atividade1/TPA/Terceiro/com/mycompany/mavenproject1/Mavenproject1.java
+++ TPA/Terceiro Bimestre/Exercicios da atividade1/TPA/Terceiro/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+package com.mycompany.mavenproject1;
  
 
  import java.util.Scanner;
